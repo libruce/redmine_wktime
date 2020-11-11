197e75fc6df55aa44be6321430897c0875bf8ca7
Mohan <nk.mohan96@gmail.com>
2020-11-11 12:06:35 +0530

    Fixed assignee issue

--- lib/timelog_helper_patch.rb
+++ lib/timelog_helper_patch.rb
@@ -45,9 +45,7 @@ module TimelogHelper
 				when "category"
 					query = get_clause(query, filter.last, "issues.category_id")
 				when "user"
-					query = query.joins("LEFT JOIN (SELECT issue_id, user_id FROM wk_issue_assignees WHERE user_id IN (#{filter.last})) AS AA ON AA.issue_id = issues.id")
-          .where("issues.assigned_to_id IN (#{filter.last}) OR AA.user_id IS NOT NULL")
-          .select("issues.id, issues.assigned_to_id, issues.estimated_hours")
+					query = get_clause(query, filter.last, "issues.assigned_to_id")
 				when "cf"
 					if filter.last.present?
 						query = query.joins(:custom_values).where({ "custom_values.customized_type": "Issue", "custom_values.custom_field_id": filter.last })
